b35022a6d0c6ac36048bc91750d9de0d6a9d039f
Carter Bodinger <carterallenbodinger@yahoo.com>
2022-12-30 13:20:21 -0500

    Update to error_bars.py to include a Two Way error bar

--- src/PyGraphing/series/error_bars.py
+++ src/PyGraphing/series/error_bars.py
@@ -22,13 +22,50 @@ class OneWay(Section):
         bars = []
 
         for i in range(len(x)):
-            bar = [('M', x[i] - w, r[i]),
-                   ('L', x[i] + w, r[i]),
+            bar = [('M', x[i] - w / 2, r[i]),
+                   ('L', x[i] + w / 2, r[i]),
                    ('M', x[i], y[i]),
                    ('L', x[i], r[i])]
             bars += bar
 
-        self.line.points = [('M', x[0], y[0]), ('L', x[1], y[1])]
+        self.line.points = bars
+        self.add_child(self.line)
+
+    def construct(self):
+        self._process()
+        return super().construct()
+
+
+class TwoWay(Section):
+    def __init__(self, parent, x: list, y: list, r: list):
+        super().__init__(0, 0)
+        self.plot = parent
+        self.line = Generic_Path()
+        self.width = 0.8
+
+        self.exes = x
+        self.whys = y
+        self.err = r
+
+    def _process(self):
+        w = self.plot.cart2pixel_x([self.width])[0]
+        x = self.plot.cart2pixel_x(self.exes)
+        y = self.plot.cart2pixel_y(self.whys)
+        r1 = self.plot.cart2pixel_y([self.whys[i] + self.err[i] for i in range(len(y))])
+        r2 = self.plot.cart2pixel_y([self.whys[i] - self.err[i] for i in range(len(y))])
+
+        bars = []
+
+        for i in range(len(x)):
+            bar = [('M', x[i] - w / 2, r1[i]),
+                   ('L', x[i] + w / 2, r1[i]),
+                   ('M', x[i] - w / 2, r2[i]),
+                   ('L', x[i] + w / 2, r2[i]),
+                   ('M', x[i], r1[i]),
+                   ('L', x[i], r2[i])]
+            bars += bar
+
+        self.line.points = bars
         self.add_child(self.line)
 
     def construct(self):
