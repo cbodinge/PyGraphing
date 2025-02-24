20b46f80875a480c96cc9458b97556b83d0af409
Carter Bodinger <carterallenbodinger@yahoo.com>
2025-02-24 11:32:46 -0600

    Adding Grouped Scatter Plots (GroupedScatter) and the ability to plot rectangles (PlotRect)

--- src/PyGraphing/series/scatter/__init__.py
+++ src/PyGraphing/series/scatter/__init__.py
@@ -4,3 +4,5 @@ from .line import Line
 from .error import ErrorBars
 from .errorB import ErrorBottom
 from .errorT import ErrorTop
+from .plot_rect import PlotRect
+from .grouped import GroupedScatter
